c07547d46b532e2ede38b5d79ca574a9cf80d684
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-31 02:43:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,30 @@
-def reverse_each_word(strings)
-  original_array = strings.split(" ")
+def reverse_each_word(string)
+  original_array = string.split(" ")
   new_array = [ ]
     original_array.each do |string|
       return new_array << string.reverse
     end
   new_array.join(" ")
 end   
+
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
+  #=> "olleH ,ereht dna woh era ?uoy"
+  
