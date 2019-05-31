aaf2e9b3ddf2561df58d1bd109046809aac2c549
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-31 02:41:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(strings)
-  new_array = strings.split(" ")
-    new_array.each do |string|
+  original_array = strings.split(" ")
+    original_array.each do |string|
       return new_array << string.reverse
     end
 end   
